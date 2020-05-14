cb5e81e3ede1a1af90579011c66cb9566327ff6e
zchyit <244939418@qq.com>
2020-05-14 18:39:42 +0800

    报告打印单构件裂缝长度

--- src/main/java/com/houses/common/model/HouseItem.java
+++ src/main/java/com/houses/common/model/HouseItem.java
@@ -287,12 +287,14 @@ public String toString(IItemCrackDao iItemCrackDao) {
 		if(crackNumCount == 1 && itemLocation == 0) {
 			List<ItemCrackVo> ItemCrackVo = iItemCrackDao.queryCrackListById(id);
 			Double crackWidth = ItemCrackVo.get(0).getMaxWidth();
-			sb = new StringBuffer(ICreatePDFServiceImpl.wallNum++ + "、" + itemSerial + itemLocationText + itemDirectionText + itemCrackTypeText + "裂缝一道,裂缝测点宽度为" + crackWidth + "mm（图" + ICreatePDFServiceImpl.textNum++ + "）。") ;
+			Double crackLength = ItemCrackVo.get(0).getMaxLength();
+			sb = new StringBuffer(ICreatePDFServiceImpl.wallNum++ + "、" + itemSerial + itemLocationText + itemDirectionText + itemCrackTypeText + "裂缝一道,裂缝测点宽度为" + crackWidth + "mm,长度为"+crackLength+ "mm（图" + ICreatePDFServiceImpl.textNum++ + "）。") ;
 			return sb.toString();
 		}else if(crackNumCount == 1) {
 			List<ItemCrackVo> ItemCrackVo = iItemCrackDao.queryCrackListById(id);
 			Double crackWidth = ItemCrackVo.get(0).getMaxWidth();
-			sb = new StringBuffer(ICreatePDFServiceImpl.wallNum++ + "、" + itemSerial + itemLocationText + itemCrackTypeText + "裂缝一道,裂缝测点宽度为" + crackWidth + "mm（图" + ICreatePDFServiceImpl.textNum++ + "）。") ;
+			Double crackLength = ItemCrackVo.get(0).getMaxLength();
+			sb = new StringBuffer(ICreatePDFServiceImpl.wallNum++ + "、" + itemSerial + itemLocationText + itemCrackTypeText + "裂缝一道,裂缝测点宽度为" + crackWidth + "mm,长度为"+crackLength+"mm（图" + ICreatePDFServiceImpl.textNum++ + "）。") ;
 			return sb.toString();
 		}
     	
